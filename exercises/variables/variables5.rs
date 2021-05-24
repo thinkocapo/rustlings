ef48ae4d96a77256dbcb2341b12756a8c54796ba
William Capozzoli <thinkocapo@gmail.com>
2021-05-23 17:34:14 -0700

    exercises/variables

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -4,8 +4,14 @@
 // I AM NOT DONE
 
 fn main() {
-    let number = "T-H-R-E-E";
+    // let spaces = "   ";
+    // let spaces = spaces.len();
+    // spaces = 234; <-- doesn't work unless add 'mut'
+    // println!("Spell a spaces : {}", spaces);
+
+    let mut number = "T-H-R-E-E";
+    let mut number = 3;
     println!("Spell a Number : {}", number);
-    number = 3;
+    number = 3; // <-- need 'mut' because reassigning here
     println!("Number plus two is : {}", number + 2);
 }
